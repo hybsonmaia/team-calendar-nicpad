029331ad924fe0b904bca06b93cf4f0055e14525
Hybson Maia <hybsonfmaia@hotmail.com>
2022-12-06 10:53:08 -0300

    att

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-var express = require('express')
-var app = express()
+// var express = require('express')
+// var app = express()
 
-//port definition
-app.set('port', process.env.PORT || 5000)
+// //port definition
+// app.set('port', process.env.PORT || 5000)
 
-app.use(express.static('webapp'))
+// app.use(express.static('webapp'))
 
-//server starting
-var server = app.listen(app.get('port'), function () {
-  var port = server.address().port
-  console.log('Express server listening on port %s', port)
+// //server starting
+// var server = app.listen(app.get('port'), function () {
+//   var port = server.address().port
+//   console.log('Express server listening on port %s', port)
+// })
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+const port = process.env.PORT || 3000
+
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+  console.log('JSON Server is running')
 })
